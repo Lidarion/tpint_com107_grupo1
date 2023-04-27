60c5ac6c976d4f2673ed34d0211a5d727a4f7181
Lisandro Capdevila <lisandrocapdevila@yahoo.com.ar>
2023-04-27 11:33:38 -0300

    se agregan algunas funcionalidades al formulario

--- js/script.js
+++ js/script.js
@@ -18,3 +18,43 @@ fetch('https://reqres.in/api/users?page=1&per_page=3')
     });
 
     //formulario del proceso 
+
+    $(document).ready(function() {
+      // paso 1 valida al 2
+      $('.next-btn').eq(0).click(function() {
+        // valida paso 1
+        if ($('#cotizacion-form')[0].checkValidity()) {
+          // Move to step 2
+          $('#paso1').addClass('d-none');
+          $('#paso2').removeClass('d-none');
+          $('.progress-bar').css('width', '66%').attr('aria-valuenow', 66).text('Paso 2 de 3');
+        } else {
+          // muestra validacion de error
+          $('#cotizacion-form')[0].reportValidity();
+        }
+      });
+      
+      // paso 2: valida y mueve al paso 1 o 3
+      $('.next-btn').eq(1).click(function() {
+        // valida paso 2
+        if ($('#cotizacion-form')[0].checkValidity()) {
+          // mueve a paso 3
+          $('#paso2').addClass('d-none');
+          $('#paso3').removeClass('d-none');
+          $('.progress-bar').css('width', '100%').attr('aria-valuenow', 100).text('Paso 3 de 3');
+        } else {
+          // muestra validacion de error
+          $('#cotizacion-form')[0].reportValidity();
+        }
+      });
+      
+      $('.prev-btn').click(function() {
+        // de vuelta a paso 1
+        $('#paso3').addClass('d-none');
+        $('#paso2').addClass('d-none');
+        $('#paso1').removeClass('d-none');
+        $('.progress-bar').css('width', '33%').attr('aria-valuenow', 33).text('Paso 1 de 3');
+      });
+    });
+    
+    
